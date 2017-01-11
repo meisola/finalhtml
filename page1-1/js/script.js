ae67279d17ffd863a7dc5b789950d66b6f8eb8e7
meisola <iamsam5566@yahoo.com.tw>
2017-01-11 22:16:39 +0800

    first commit

--- page1-1/js/script.js
+++ page1-1/js/script.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+
+  $(".ic").click(function(){   /*漢堡選單*/
+    if( $(".ic").hasClass("click") ){
+      $(".ic").removeClass("click");
+      $(".site-nav").slideUp();
+    }else{
+      $(".ic").addClass("click");
+      $(".site-nav").slideDown();
+      return false;
+    }
+  });
+  $(".ic").mouseleave(function(){
+    if( $(this).hasClass("click") ){
+      $(".site-nav").slideDown();
+      $(this).removeClass("click");
+    }
+
+  });
+
+
+});
+
+
+
  $(document).ready(function(){
 	 $(".ban-pic1-1 img").hover(
 	    function() {
